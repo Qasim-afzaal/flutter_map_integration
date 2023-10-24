090805d96d338537f5955cd36dacf7dccaa3b534
QasimALi <83086966+qasim1221@users.noreply.github.com>
2023-10-24 20:13:47 +0500

    Structure add

---
+++ lib/Screens/main_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({super.key});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text("Google Map"),
+      ),
+
+      body: Column(children: [
+        
+
+      ]),
+    );
+  }
+}
